8edb5b4c34bc608dc33dcefd39809b2d0d36eab6
LinDragon1002 <lindragon1002@gmail.com>
2024-11-27 20:30:59 +0800

    update ToDoListSystem.js 完成排序和格式化

--- ITSA practice/ToDoListSystem.js
+++ ITSA practice/ToDoListSystem.js
@@ -7,39 +7,34 @@ function datetimeformate (date) {
     const time_control = Math.floor(date.getHours()/12)
     const time_path = ["AM","PM"]
     const months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
-    return `${months[date.getMonth()+1]} ${date.getDate()},${date.getFullYear()} at ${(date.getHours()%12).toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')} ${time_path[time_control]}`;
+    return `${months[date.getMonth()]} ${date.getDate()},${date.getFullYear()} at ${(date.getHours()%12).toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')} ${time_path[time_control]}`;
 }
 
 function sorttime (containerseletor){
     const container = document.querySelector(containerseletor)
     const items = Array.from(container.querySelectorAll("li"))
     items.sort((a,b) => {
-        const dataA = new Date(a.querySelector('.datetime').textContent.trim())
-        console.log(dataA)
-        const dataB = new Date(b.querySelector(".datetime").textContent.trim())
-        console.log(dataB)
+        const dataA = new Date(a.querySelector("i").dataset.time).getTime()
+        const dataB = new Date(b.querySelector("i").dataset.time).getTime()
         return dataA - dataB
     })
     container.innerHTML = ''
-    items.forEach(item => {
-        const dateElement = item.querySelector(".datetime");
-        const rawDate = new Date(dateElement.textContent.trim());
-        dateElement.textContent = datetimeformate(rawDate);
-        container.appendChild(item);
-    });
+    items.forEach(item => {container.appendChild(item);});
 }
 
 function newlist() {
     const title = document.querySelector("#title").value
     const content = document.querySelector("#content").value
+    const datetime = document.querySelector("#datetime").value
+    const rawDatetime = new Date(datetime)
+
     const data_id = document.querySelector(".nonefinish > ul")
-    const datetime = new Date(document.querySelector("#datetime").value)
     data_id.innerHTML +=
     `
     <li class="data-id${++id}">
         <span class="title"><strong>${title}</strong></span><br>
-        <span class="content">${content}</span><br>
-        <span class="datetime"><i>${datetimeformate(datetime)}</i></span><br>
+        <span>${content}</span><br>
+        <span><i data-time="${rawDatetime}">${datetimeformate(rawDatetime)}</i></span><br>
         <button class="removebtn" onclick="removelist(${id})">刪除</button>
         <button class="finishbtn" onclick="finishlist(${id})">完成</button>
     </li>
@@ -50,7 +45,6 @@ function newlist() {
 function removelist(id){
     const li = document.querySelectorAll(`.data-id${id}`)
     li.forEach(index => {
-        console.log(index)
         index.remove()
     })
 }
@@ -61,6 +55,7 @@ function finishlist(id){
     const finishButton = li.querySelector(".finishbtn");
     finishButton.remove()
     finish.append(li)
+    sorttime(".finish ul")
 }
 
 
