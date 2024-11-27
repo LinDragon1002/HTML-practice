ca7cb5f94bae0df6c48eb5ea5b26fe02a5ed44d9
LinDragon1002 <lindragon1002@gmail.com>
2024-11-27 16:36:41 +0800

    update ToDoListSystem.html and ToDoListSystem.js 完成按鈕移除和初步時間格式

--- ITSA practice/ToDoListSystem.js
+++ ITSA practice/ToDoListSystem.js
@@ -3,32 +3,42 @@ const removebtn = document.querySelector(".removebtn")
 const finishbtn = document.querySelector(".finishbtn")
 let id = 0;
 
+function datetimeformate () {
+    const alltime = new Date (document.querySelector("#datetime").value)
+    const time_control = Math.floor(alltime.getHours()/12)
+    const time_path = ["AM","PM"]
+    let formatetime = `${alltime.getMonth()+1} ${alltime.getDate()},${alltime.getFullYear()} at ${alltime.getHours()%12}:${alltime.getMinutes()} ${time_path[time_control]}`
+    return formatetime;
+}
+
 function newlist() {
     const title = document.querySelector("#title").value
     const content = document.querySelector("#content").value
     const datetime = document.querySelector("#datetime").value
-    nonefinish.innerHTML +=
+    const data_id = document.querySelector(".nonefinish > ul")
+    data_id.innerHTML +=
     `
-    <ul class="data-id${++id}">
-        <li class="title"><strong>${title}</strong></li>
-        <li class="content">${content}</li>
-        <li class="datatime"><i>${datetime}</i></li>
-        <button class="removebtn" onclick="removelist(${id})">刪除</button>
-        <button class="finishbtn" onclick="finishlist(${id})">完成</button>
-    </ul>
+    <li class="data-id${++id}">
+        <span class="title"><strong>${title}</strong></span><br>
+        <span class="content">${content}</span><br>
+        <span class="datetime"><i>${datetimeformate()}</i></span><br>
+        <button class="removebtn" onclick="removelist()">刪除</button>
+        <button class="finishbtn" onclick="finishlist()">完成</button>
+    </li>
     `
 }
 
-function removelist(id){
-    const ul = document.querySelector(`.data-id${id}`)
-    ul.remove()
+function removelist(){
+    const li = document.querySelector(`.data-id${id}`)
+    li.remove()
 }
 
-function finishlist(id){
-    const finish = document.querySelector(".finish")
-    const ul = document.querySelector(`.data-id${id}`)
-    finish.append(ul)
-    ul.lastChild.remove(finishbtn)
+function finishlist(){
+    const finish = document.querySelector(".finish > ul")
+    const li = document.querySelector(`.data-id${id}`)
+    const finishButton = li.querySelector(".finishbtn");
+    finishButton.remove()
+    finish.append(li)
 }
 
 
