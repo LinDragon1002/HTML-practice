6f12bf48530010c10cc4a6191aa0d4d931282b5d
LinDragon1002 <lindragon1002@gmail.com>
2024-12-26 12:54:41 +0800

    update drink_system.htm .css .js
    完成訂單新增、刪除，但是結帳功能和必填表單尚未完成

--- ITSA practice/drink_system.js
+++ ITSA practice/drink_system.js
@@ -1,13 +1,28 @@
 const drinkname = document.querySelector('.name')
 const size = document.querySelector('.size')
 const price = document.querySelector('.price')
-const count = document.querySelector('.count')
+const count = document.querySelector('.count > input')
 const total = document.querySelector('.total')
 const temp = document.querySelector('.temp')
 const sweet = document.querySelector('.sweet')
-const person = document.querySelector('.person')
+const person = document.querySelector('.person > input')
+const show = document.getElementById('show')
+const p = document.querySelector('.btnbox > p')
 let prices = 0
+let id = 0
 
+class Drink {
+    constructor(drinkname, size, price, count, total, temp, sweet, person) {
+        this.drinkname = drinkname
+        this.size = size
+        this.price = price
+        this.count = count
+        this.total = total
+        this.temp = temp
+        this.sweet = sweet
+        this.person = person
+    }
+}
 
 drinkname.addEventListener('change', function() {
     if (drinkname[drinkname.selectedIndex].textContent ==  '紅茶') {
@@ -32,16 +47,76 @@ size.addEventListener('change', function() {
 })
 
 count.addEventListener('input', function() {
-    console.log(count.textContent)
+    console.log(count.value)
+    if (isNaN(count.value)) {
+        count.value = 0
+    }
     total.innerHTML = prices * count.value
 })
 
+function clearinp() {
+    drinkname.selectedIndex = 0
+    size.selectedIndex = 0
+    price.innerHTML = 0
+    total.innerHTML = 0
+    count.value = 0
+    temp.selectedIndex = 0
+    sweet.selectedIndex = 0
+    person.value = ''
+}
 
-
-
-
-
+function totalpricescount() {
+    const alltr = document.querySelectorAll('.ans > tbody > tr')
+    let temp = 0
+    alltr.forEach((name,index) => {
+        if (index > 0){
+            temp += parseInt(name.children[5].textContent)
+        }
+    })
+    p.innerHTML = `總計:${alltr.length-1}份${temp}元`
+}
 
 function sumbit() {
+    show.style.display = 'flex'
+    const drink = new Drink(drinkname[drinkname.selectedIndex].textContent,
+                            size[size.selectedIndex].textContent,
+                            prices,
+                            count.value,
+                            total.innerHTML,
+                            temp[temp.selectedIndex].textContent,
+                            sweet[sweet.selectedIndex].textContent,
+                            person.value)
+    const ans = document.querySelector('.ans > tbody')
+    ans.innerHTML +=
+    `
+    <tr class="data-id${++id}">
+        <td><input type="checkbox" name="" class="checkbox data-id${id}"></td>
+        <td>${drink.drinkname}</td>
+        <td>${drink.size}</td>
+        <td>${prices}</td>
+        <td>${drink.count}</td>
+        <td>${drink.total}</td>
+        <td>${drink.temp}</td>
+        <td>${drink.sweet}</td>
+        <td>${drink.person}</td>
+    </tr>
+    `
+    totalpricescount()
+    clearinp()
+}
 
+
+function del() {
+    const checkbox = document.querySelectorAll('.checkbox')
+    checkbox.forEach(name => {
+        if (name.checked) {
+            const data = document.querySelector('.data-id' + name.classList[1].slice(7))
+            data.remove()
+        }
+    });
+    totalpricescount()
+}
+
+function buy(){
+    print(document.querySelector('#show > table'))
 }
